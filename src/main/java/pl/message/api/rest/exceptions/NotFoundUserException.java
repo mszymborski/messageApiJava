6aa0bf52bf9d8ca217446f712b91946d9a34c8cb
mar14 <19radiogra@gmail.com>
2018-11-21 01:02:21 +0100

    Add use of parameter map

--- src/main/java/pl/message/api/rest/exceptions/NotFoundUserException.java
+++ src/main/java/pl/message/api/rest/exceptions/NotFoundUserException.java
@@ -1,10 +1,14 @@
 package pl.message.api.rest.exceptions;
 
+import java.util.Map;
+
 public class NotFoundUserException extends Exception {
 
-	private Long id;
+    public NotFoundUserException() {
+        super("Not found any user");
+    }
 
-	public NotFoundUserException(Long id) {
-		super("NotFoundUserException: Not found user with id = " + id);
+    public NotFoundUserException(Map<String,String> parametersMap) {
+		super("NotFoundUserException: Not found user for: " + parametersMap);
 	}
 }
